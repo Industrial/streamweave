798919840fcaafb709e45e5f1793034b96799c7a
tom <tom.wieland@gmail.com>
2025-04-12 21:24:26 +0000

    fix(effect-stream-basic): disable old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-pub mod consumers;
+// pub mod consumers;
 // pub mod effect;
-pub mod error;
-pub mod pipeline;
-pub mod producers;
-pub mod traits;
-pub mod transformers;
+// pub mod error;
+// pub mod pipeline;
+// pub mod producers;
+// pub mod traits;
+// pub mod transformers;
